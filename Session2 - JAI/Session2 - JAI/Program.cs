22a4c47e60193eda5ea4581b05e3dab3697e6010
jennaiuppa <jaiuppa@me.com>
2016-10-06 19:13:26 -0700

    These are programs from class (first csharp projects)

---
+++ Session2 - JAI/Session2 - JAI/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class MyLittleProject
+{
+    static void Main()//MY ENTRY TO THE PROGRAM
+    {
+        Console.WriteLine("Hello World!");
+        Console.ReadLine();
+    }
+}//END OF MY CLASS
