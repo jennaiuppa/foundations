554b1a6e9b681adbe8130d67a6506ca000bc0ad2
jennaiuppa <jaiuppa@me.com>
2016-10-31 18:27:50 -0700

    Homework Assignment 3

---
+++ Homework3/Homework3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+
+        //declare values
+        {
+            int n, number, result;
+
+            //ask to enter number
+            Console.Write("Please enter a small number to calculate the Factorial ");
+            number = int.Parse(Console.ReadLine());
+            result = number;
+
+
+            //calculate factorial and save
+            for (n = number - 1; n >= 1; n--)
+
+
+            {//start of loop
+                result = result * n;
+            }//end of loop
+
+
+            //show result
+            Console.Write("The facortial is {0} ", result);
+
+            //hold result
+            Console.ReadLine();
+        }
+    }
+}
