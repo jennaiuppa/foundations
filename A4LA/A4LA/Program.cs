dcecd32eefd5bf1947577d72932131ac90403793
jennaiuppa <jaiuppa@me.com>
2016-12-09 14:39:15 -0800

    Final Assignment 4

---
+++ A4LA/A4LA/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n, fact = 1, result;
+
+        Console.Write("Enter a small number of factorials you wish to display ");
+        result = int.Parse(Console.ReadLine());
+
+
+        for (n = 1; n <= result; n++)
+        {
+            fact = fact * n;
+            Console.WriteLine("{0}!={1}", n, fact);
+
+        }
+        Console.ReadLine();
+    }
+}
